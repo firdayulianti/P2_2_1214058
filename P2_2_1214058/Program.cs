7b8d24a5c2d8dd3b457c22c098e9bf318ea20b84
Firda yulianti <Firda yulianti@LAPTOP-KIF3SKT8>
2022-10-02 23:32:26 +0700

    Add project files.

---
+++ P2_2_1214058/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_2_1214058
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello!!");
+            Console.Write("Silahkan berhitung");
+
+            int gajipokok = int.Parse(Console.ReadLine());
+            int tunjangan = (gajipokok * 20) / 100;
+            int bonus = (gajipokok * 15) / 100;
+            double pph = (gajipokok * 3.5) / 100;
+            double totalgaji = (gajipokok + tunjangan + bonus);
+            double gajibersih = (gajipokok + tunjangan + bonus) - pph;
+
+            Console.Write("Gaji pokok = " + gajipokok);
+            Console.Write("Tunjangan = " + tunjangan);
+            Console.Write("Bonus = " + bonus);
+            Console.Write(" PPH + = " + pph);
+            Console.Write("Total gaji = " + totalgaji);
+            Console.Write("Gaji bersih = " + gajibersih);
+        }
+    }
+}
